202c74544da2a3b309c663cd8f7ffea304aaf21d
Francis Tsiboe <ftsiboe@wizard24.beocat.ksu.edu>
2025-12-13 21:26:24 -0600

    updates

--- data-raw/scripts/003_render_rate.R
+++ data-raw/scripts/003_render_rate.R
@@ -13,65 +13,59 @@ if (Sys.info()['sysname'] %in% "Windows") {
 output_directory  <- study_environment$wd$redesigns
 if(!dir.exists(output_directory)) dir.create(output_directory, recursive = TRUE)
 
-design_specs <- c(200,seq(5,60,1))
-
 done_list <- basename(list.files(output_directory,full.names = TRUE, recursive = TRUE))
+table(done_list[grepl("prf_index_",done_list)])
 done_list <- data.frame(name=done_list[!grepl("prf_index_",done_list)])
 done_list <- tidyr::separate(done_list,"name",sep="_",into = c("prf","rates","range","year"))
 table(done_list$range,done_list$year)
 
 #table(done_list$range)
 
+design_specs <- data.table::CJ(
+  history_range = c(200,seq(5,60,1)),
+  commodity_year = 2016:current_year,
+  unique = TRUE)
+
+# If running under a SLURM array job, filter the design_specs by the current task ID
 if (!is.na(as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")))) {
-  design_specs <- design_specs[as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))]
+  # Replicate the range [TASK_MIN:TASK_MAX] until it matches the length of design_specs
+  design_specs$task <- rep(
+    as.numeric(Sys.getenv("SLURM_ARRAY_TASK_MIN")):as.numeric(Sys.getenv("SLURM_ARRAY_TASK_MAX")),
+    length = nrow(design_specs)
+  )
+  # Keep only rows assigned to this specific SLURM_ARRAY_TASK_ID
+  design_specs <- design_specs[design_specs$task %in% as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")), ]
 }
 
 lapply(
-  design_specs,
-  function(history_range){
+  1:nrow(design_specs),
+  function(i){
     tryCatch({
-      # history_range <- 200
+      # i <- 1
+      year           <- design_specs$commodity_year[i]
+      history_range  <- design_specs$history_range[i]
+      range_tag      <- stringr::str_pad(history_range,pad="0",3)
+      out_file_rates <- file.path(output_directory, range_tag,paste0("prf_rates_",range_tag,"_",year,".rds"))
 
-      range_tag <- stringr::str_pad(history_range,pad="0",3)
+      if(!file.exists(out_file_rates)){
+        prf_index_data <- readRDS(file.path(output_directory, range_tag,paste0("prf_index_",range_tag,".rds")))
+        # prf_index_data <- prf_index_data[grid_id %in% unique(prf_index_data$grid_id)[1:10]]
+        # Compute PRF Premium Rates Across Program Years and Coverage Levels
 
-      history_range_directory <- file.path(output_directory, range_tag)
-
-      if(!dir.exists(history_range_directory)) dir.create(history_range_directory, recursive = TRUE)
-
-      out_file_index <- file.path(history_range_directory,paste0("prf_index_",range_tag,".rds"))
-
-      prf_index_data <- readRDS(out_file_index)
-      # prf_index_data <- prf_index_data[grid_id %in% unique(prf_index_data$grid_id)[1:10]]
-      # Compute PRF Premium Rates Across Program Years and Coverage Levels
-
-      plan(sequential)
-      plan(list(tweak(multisession, workers = available_cores)));gc()
-
-      future_lapply(
-        study_environment$year_beg:current_year,
-        function(year){
-          tryCatch({
-            out_file_rates <- file.path(history_range_directory,paste0("prf_rates_",range_tag,"_",year,".rds"))
-            if(!file.exists(out_file_rates)){
-              prf_rate_data <- compute_prf_premium_rate(
-                program_years   = year,
-                prf_index_data  = prf_index_data,
-                identifiers     = c("index_history_range","grid_id","interval_code"),
-                year_col        = "commodity_year",
-                index_value_col = "index")
-              saveRDS(prf_rate_data, out_file_rates)
-              rm(prf_rate_data);gc()
-            }
-            invisible()
-            }, error = function(e){invisible()})
-        })
-      plan(sequential)
-
-      rm(prf_index_data);gc()
+        prf_rate_data <- compute_prf_premium_rate(
+          program_years   = year,
+          prf_index_data  = prf_index_data,
+          identifiers     = c("index_history_range","grid_id","interval_code"),
+          year_col        = "commodity_year",
+          index_value_col = "index")
+        saveRDS(prf_rate_data, out_file_rates)
+        rm(prf_rate_data,prf_index_data);gc()
+      }
       invisible()
 
     }, error = function(e){invisible()})
   })
+
 
 # Upload the assets
 function(){
