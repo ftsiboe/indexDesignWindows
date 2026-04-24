f5625c2e54ec0d2729c27f1ec0005fd35e70c7da
Francis Tsiboe <69377090+ftsiboe@users.noreply.github.com>
2026-04-24 11:16:04 -0500

    Update 101_figures_precipitation_trend.R

--- data-raw/scripts/101_figures_precipitation_trend.R
+++ data-raw/scripts/101_figures_precipitation_trend.R
@@ -9,12 +9,14 @@ redesigns_directory  <- study_environment$wd$redesigns
 prf_grid_weights <- readRDS("data/prf_grid_weights.rds")
 Keep.List<-c("Keep.List",ls())
 
-data <- readRDS("data-raw/releases/outputs/county_precipitation_trend.rds")
+data <- readRDS("data-raw/output/county_precipitation_trend.rds")
 unique(data$trend_class)
 
+data <- data[model_estimator %in% "random" & precipitation %in% "log"]
+# "random","pooling","within"
 fig_trend <- plot_prf_statistics(
   data = data[history_range %in% c(5,10,15,20,25,30,35,40,45,50,55,60,200)],
-  outcome_variable = "trend_class",
+  outcome_variable = "trend_class05",
   plot_title = NULL,
   spatial_unit ="county",
   palette = c(
