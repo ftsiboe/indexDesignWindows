903d80e60a2e94ffb11f6b17d3c4aa93526b090f
Francis Tsiboe <ftsiboe@warlock17.beocat.ksu.edu>
2026-04-27 17:27:14 -0500

    updates

--- data-raw/scripts/101_figures_precipitation_trend.R
+++ data-raw/scripts/101_figures_precipitation_trend.R
@@ -12,28 +12,57 @@ Keep.List<-c("Keep.List",ls())
 data <- readRDS("data-raw/output/county_precipitation_trend.rds")
 # unique(data$trend_class05)
 
-data <- data[model_estimator %in% "pooling"]
+unique(data$trend_class_05)
+
+data <- data[model_estimator %in% "within"]
+data <- data[!trend_class_05 %in% NA]
+data[
+  ,trend_class := factor(
+    trend_class_05,
+    c("Positive Mean and Positive Variance",
+      "Positive Mean and Negative Variance",
+      "Negative Mean and Positive Variance",
+      "Negative Mean and Negative Variance",
+      "Positive Mean only",
+      "Negative Mean only",
+      "Positive Variance only",
+      "Negative Variance only",
+      "None"))]
+
+
+trend_palette <- c(
+  "Positive Mean and Positive Variance" = "#00583D",  # strong warm
+  "Positive Mean and Negative Variance" = "#FFC425",  # warm yellow
+  "Negative Mean and Positive Variance" = "purple",   # mixed negative/positive
+  "Negative Mean and Negative Variance" = "#B83E27",  # strong cool/dark green
+
+  "Positive Mean only"                 = "#F4A261",  # lighter warm
+  "Negative Mean only"                 = "#9DD9F7",  # light cool
+
+  "Positive Variance only"             = "#E76F51",  # medium warm
+  "Negative Variance only"             = "#2A9D8F",  # medium cool-green
+
+  "None"                               = "#BDBDBD"   # neutral gray
+)
+
 # "random","pooling","within"
 fig_trend <- plot_prf_statistics(
   data = data[history_range %in% c(5,10,15,20,25,30,35,40,45,50,55,60,200)],
-  outcome_variable = "trend_class05",
+  outcome_variable = "trend_class",
   plot_title = NULL,
   spatial_unit ="county",
-  palette = c(
-    "firebrick",
-    "#9DD9F7",
-    "#00583D"
-  )) +
-  guides(fill = guide_legend(nrow = 1)) +
+  palette = trend_palette) +
+  guides(fill = guide_legend(ncol = 1)) +
   theme(
-    legend.position = "top",
+    legend.position = c(0.60, 0.10),
     legend.key.size = unit(0.25, "cm"),
     legend.text = element_text(size = 7),
     plot.title = element_text(size = 9, hjust = 0.5)
-  ) + facet_wrap(~history_range,nrow=4)
+  ) + facet_wrap(~history_range,ncol=3)
+
 
 ggsave(file.path("data-raw/output/figure","county_precipitation_trend.png"),
-       fig_trend, dpi = 600,width = 8, height =5)
+       fig_trend, dpi = 600,width = 6.5, height =7.1)
 
 
 
